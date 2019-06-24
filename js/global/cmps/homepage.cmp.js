243d85b4c44cac2a72c634d11477b65087794fd0
Or Biton <orbiton2007@gmail.com>
2019-06-24 19:51:36 +0300

    //

--- js/global/cmps/homepage.cmp.js
+++ js/global/cmps/homepage.cmp.js
@@ -5,8 +5,9 @@ export default {
        
         <div class="imgs-home flex space-around">
             <h1 class="hinge">AppSus</h1>
-            <!-- <img class="img-mail" @click="goMail" src="css/email-css/images/emailmenu.png">
-            <img class="img-note" @click="goNote" src="css/email-css/images/document.png"> -->
+            <!-- <img class="img-mail" @click="goMail" src="css/email-css/images/emailmenu.png"> -->
+            <!-- <img class="img-note" @click="goNote" src="css/email-css/images/document.png"> -->
+            <img class="img-note" src="img/horse.png">
         </div>
 
     </section>
