0d9eabb3ed26792afffc8b2a63683b4be48362dd
Or Biton <orbiton2007@gmail.com>
2019-06-24 19:42:25 +0300

    //

--- js/global/cmps/homepage.cmp.js
+++ js/global/cmps/homepage.cmp.js
@@ -1,6 +1,22 @@
 
 export default {
     template:`
-        <h1>Home</h1>
-    `
+    <section class="sec-home-container">
+       
+        <div class="imgs-home flex space-around">
+            <h1 class="hinge">AppSus</h1>
+            <!-- <img class="img-mail" @click="goMail" src="css/email-css/images/emailmenu.png">
+            <img class="img-note" @click="goNote" src="css/email-css/images/document.png"> -->
+        </div>
+
+    </section>
+    `,
+    methods:{
+        goMail(){
+            this.$router.push('/emails')
+        },
+        goNote(){
+            this.$router.push('/notes')
+        }
+    }
 }
