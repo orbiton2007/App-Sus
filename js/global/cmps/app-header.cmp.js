493e2148a74d6aa080e15855df998153ef2e6645
Or Biton <orbiton2007@gmail.com>
2019-06-24 18:39:15 +0300

    //

--- js/global/cmps/app-header.cmp.js
+++ js/global/cmps/app-header.cmp.js
@@ -5,9 +5,9 @@ export default {
         <section class="header-container">
             <div>
                 <img class="icon-burger" src="css/email-css/images/burger.png" @click="toggleMenu">
-                <img class="header-btn" :class="{'icon-homepage': showMenu}" @click="goHome" src="css/email-css/images/homepage.png">
-                <img class="header-btn" :class="{'icon-email': showMenu}" @click="goMail" src="css/email-css/images/emailmenu.png">
-                <img class="header-btn" :class="{'icon-note': showMenu}" @click="goNote" src="css/email-css/images/document.png">
+                <img class="header-btn-burger" :class="{'icon-homepage': showMenu}" @click="goHome" src="css/email-css/images/homepage.png">
+                <img class="header-btn-burger" :class="{'icon-email': showMenu}" @click="goMail" src="css/email-css/images/emailmenu.png">
+                <img class="header-btn-burger" :class="{'icon-note': showMenu}" @click="goNote" src="css/email-css/images/document.png">
             </div>
         </section>
     `,
