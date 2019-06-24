a7caf026543202ebee7e7588bc14950c9403e2ae
Or Biton <orbiton2007@gmail.com>
2019-06-24 18:34:46 +0300

    //

--- js/global/cmps/app-header.cmp.js
+++ js/global/cmps/app-header.cmp.js
@@ -1,11 +1,35 @@
 
 
 export default {
-    template:`
+    template: `
         <section class="header-container">
-            <router-link to="/">Home</router-link>
-            <router-link to="/emails">App Email</router-link>
-            <router-link to="/notes">App Note</router-link>
+            <div>
+                <img class="icon-burger" src="css/email-css/images/burger.png" @click="toggleMenu">
+                <img class="header-btn" :class="{'icon-homepage': showMenu}" @click="goHome" src="css/email-css/images/homepage.png">
+                <img class="header-btn" :class="{'icon-email': showMenu}" @click="goMail" src="css/email-css/images/emailmenu.png">
+                <img class="header-btn" :class="{'icon-note': showMenu}" @click="goNote" src="css/email-css/images/document.png">
+            </div>
         </section>
-    `
+    `,
+    data() {
+        return {
+            showMenu: false
+        }
+    },computed:{
+        
+    },
+    methods:{
+        toggleMenu(){
+            this.showMenu = !this.showMenu;
+        },
+        goHome(){
+            this.$router.push('/')
+        },
+        goMail(){
+            this.$router.push('/emails')
+        },
+        goNote(){
+            this.$router.push('/notes')
+        }
+    }
 }
