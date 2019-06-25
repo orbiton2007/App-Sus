e0e3487072e00edd9dee01e816897309f96313e3
Or Biton <orbiton2007@gmail.com>
2019-06-25 03:52:17 +0300

    ///

--- js/global/cmps/homepage.cmp.js
+++ js/global/cmps/homepage.cmp.js
@@ -4,10 +4,12 @@ export default {
     <section class="sec-home-container">
        
         <div class="imgs-home flex space-around">
-            <h1 class="hinge">AppSus</h1>
+            <h1 class="heartBeat">AppSus</h1>
             <!-- <img class="img-mail" @click="goMail" src="css/email-css/images/emailmenu.png"> -->
             <!-- <img class="img-note" @click="goNote" src="css/email-css/images/document.png"> -->
-            <img class="img-note" src="img/horse.png">
+            <!-- <img class="img-note" src="img/horse.png"> -->
+            <img tabindex="1" class="Img-horse"
+            src="http://cdn.lookanimals.com/pictures/cliparts.co/cliparts/8Tz/Byj/8TzByjyTp.gif" alt="Running Horse"  >
         </div>
 
     </section>
